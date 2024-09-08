8d0e87f353efdf394cc33015903d0fa4f46d5642
Huy Duong <huy.duongdinh@gmail.com>
2024-09-08 13:40:15 +0700

    Init qmk_userspace

---
+++ keyboards/crkbd/keymaps/dinhhuy258/rules.mk
@@ -0,0 +1,8 @@
+MCU = RP2040
+BOOTLOADER = rp2040
+CONVERT_TO = rp2040_ce
+
+OLED_ENABLE = yes
+# Enables Link Time Optimization (LTO) when compiling the keyboard.
+# This makes the process take longer, but it can significantly reduce the compiled size.
+LTO_ENABLE = yes

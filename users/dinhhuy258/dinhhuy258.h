6c61abf0be660927ecfec85640483cce44b918e3
Huy Duong <huy.duongdinh@gmail.com>
2024-09-12 18:14:09 +0700

    Number layer

--- users/dinhhuy258/dinhhuy258.h
+++ users/dinhhuy258/dinhhuy258.h
@@ -2,6 +2,9 @@
 
 #include QMK_KEYBOARD_H
 
+#define TMUX_PREFIX LCTL(KC_F)
+#define VIM_PREFIX KC_SCLN
+
 enum layers {
     BASE = 0,
     NAV,
@@ -23,7 +26,7 @@ enum layers {
 
 #define ___________________NAV_L1__________________       KC_BRID,         KC_BRIU,         HYPR(KC_4),      XXXXXXX,         XXXXXXX
 #define ___________________NAV_L2__________________       KC_LGUI,         KC_LOPT,         KC_LCTL,         KC_LSFT,         XXXXXXX
-#define ___________________NAV_L3__________________       KC_VOLD,         KC_VOLU,         KC_MUTE,         LCTL(KC_F),      XXXXXXX
+#define ___________________NAV_L3__________________       KC_VOLD,         KC_VOLU,         KC_MUTE,         TMUX_PREFIX,     XXXXXXX
 #define ______________NAV_THUMB_LEFT_______________       _______,         _______
 
 #define ___________________NAV_R1__________________       XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,         LSG(KC_TAB)
@@ -51,12 +54,12 @@ enum layers {
 #define ___________________SYM_R3__________________       KC_QUOT,         KC_DQUO,         KC_COMM,         KC_DOT,          KC_SLSH
 #define ______________SYM_THUMB_RIGHT______________       _______,         _______
 
-#define ___________________NUM_L1__________________       KC_Q,            KC_W,            KC_E,            KC_R,            KC_T
-#define ___________________NUM_L2__________________       LGUI_T(KC_A),    LALT_T(KC_S),    LCTL_T(KC_D),    LSFT_T(KC_F),    HYPR_T(KC_G)
-#define ___________________NUM_L3__________________       KC_Z,            KC_X,            KC_C,            KC_V,            KC_B
-#define ______________NUM_THUMB_LEFT_______________       KC_ESC,          KC_SPC
+#define ___________________NUM_L1__________________       KC_MINS,         KC_7,            KC_8,            KC_9,            KC_PLUS
+#define ___________________NUM_L2__________________       KC_BSLS,         KC_4,            KC_5,            KC_6,            KC_ASTR
+#define ___________________NUM_L3__________________       KC_0,            KC_1,            KC_2,            KC_3,            KC_EQL
+#define ______________NUM_THUMB_LEFT_______________       _______,         _______
 
-#define ___________________NUM_R1__________________       KC_Y,            KC_U,            KC_I,            KC_O,            KC_P
-#define ___________________NUM_R2__________________       HYPR_T(KC_H),    LSFT_T(KC_J),    LCTL_T(KC_K),    LALT_T(KC_L),    LGUI_T(KC_SCLN)
-#define ___________________NUM_R3__________________       KC_N,            KC_M,            KC_COMM,         KC_DOT,          KC_SLSH
-#define ______________NUM_THUMB_RIGHT______________       KC_ENT,          KC_BSPC
+#define ___________________NUM_R1__________________       XXXXXXX,         VIM_PREFIX,      XXXXXXX,         XXXXXXX,         KC_PERC
+#define ___________________NUM_R2__________________       XXXXXXX,         KC_LSFT,         KC_LCTL,         KC_LOPT,         KC_LGUI
+#define ___________________NUM_R3__________________       XXXXXXX,         TMUX_PREFIX,     KC_COMM,         KC_DOT,          KC_SLSH
+#define ______________NUM_THUMB_RIGHT______________       _______,         _______

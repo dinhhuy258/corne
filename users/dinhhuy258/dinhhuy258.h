b81d6f3ea21d8e61ffd73e49cc3162e3030f6936
Huy Duong <huy.duongdinh@gmail.com>
2024-09-12 17:44:17 +0700

    Update navigation layer

--- users/dinhhuy258/dinhhuy258.h
+++ users/dinhhuy258/dinhhuy258.h
@@ -10,25 +10,26 @@ enum layers {
     NUM,
 };
 
+// clang-format off
 #define __________________BASE_L1__________________       KC_Q,            KC_W,            KC_E,            KC_R,            KC_T
-#define __________________BASE_L2__________________       LGUI_T(KC_A),    LALT_T(KC_S),    LCTL_T(KC_D),    LSFT_T(KC_F),    HYPR_T(KC_G)
+#define __________________BASE_L2__________________       LGUI_T(KC_A),    LOPT_T(KC_S),    LCTL_T(KC_D),    LSFT_T(KC_F),    HYPR_T(KC_G)
 #define __________________BASE_L3__________________       KC_Z,            KC_X,            KC_C,            KC_V,            KC_B
-#define ______________BASE_THUMB_LEFT______________       KC_ESC,          KC_SPC
+#define ______________BASE_THUMB_LEFT______________       LT(NAV, KC_ESC), LT(MOU, KC_SPC)
 
 #define __________________BASE_R1__________________       KC_Y,            KC_U,            KC_I,            KC_O,            KC_P
 #define __________________BASE_R2__________________       HYPR_T(KC_H),    LSFT_T(KC_J),    LCTL_T(KC_K),    LALT_T(KC_L),    LGUI_T(KC_SCLN)
 #define __________________BASE_R3__________________       KC_N,            KC_M,            KC_COMM,         KC_DOT,          KC_SLSH
-#define ______________BASE_THUMB_RIGHT_____________       KC_ENT,          KC_BSPC
+#define ______________BASE_THUMB_RIGHT_____________       LT(NUM, KC_ENT), LT(SYM, KC_BSPC)
 
-#define ___________________NAV_L1__________________       KC_Q,            KC_W,            KC_E,            KC_R,            KC_T
-#define ___________________NAV_L2__________________       LGUI_T(KC_A),    LALT_T(KC_S),    LCTL_T(KC_D),    LSFT_T(KC_F),    HYPR_T(KC_G)
-#define ___________________NAV_L3__________________       KC_Z,            KC_X,            KC_C,            KC_V,            KC_B
-#define ______________NAV_THUMB_LEFT_______________       KC_ESC,          KC_SPC
+#define ___________________NAV_L1__________________       KC_BRID,         KC_BRIU,         HYPR(KC_4),      XXXXXXX,         XXXXXXX
+#define ___________________NAV_L2__________________       KC_LGUI,         KC_LOPT,         KC_LCTL,         KC_LSFT,         XXXXXXX
+#define ___________________NAV_L3__________________       KC_VOLD,         KC_VOLU,         KC_MUTE,         LCTL(KC_F),      XXXXXXX
+#define ______________NAV_THUMB_LEFT_______________       _______,         _______
 
-#define ___________________NAV_R1__________________       KC_Y,            KC_U,            KC_I,            KC_O,            KC_P
-#define ___________________NAV_R2__________________       HYPR_T(KC_H),    LSFT_T(KC_J),    LCTL_T(KC_K),    LALT_T(KC_L),    LGUI_T(KC_SCLN)
-#define ___________________NAV_R3__________________       KC_N,            KC_M,            KC_COMM,         KC_DOT,          KC_SLSH
-#define ______________NAV_THUMB_RIGHT______________       KC_ENT,          KC_BSPC
+#define ___________________NAV_R1__________________       XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,         LSG(KC_TAB)
+#define ___________________NAV_R2__________________       KC_LEFT,         KC_DOWN,         KC_UP,           KC_RGHT,         LGUI(KC_TAB)
+#define ___________________NAV_R3__________________       KC_F23,          LSFT(KC_LEFT),   LSFT(KC_RIGHT),  KC_F24,          LGUI(KC_GRAVE)
+#define ______________NAV_THUMB_RIGHT______________       _______,          _______
 
 #define ___________________MOU_L1__________________       KC_Q,            KC_W,            KC_E,            KC_R,            KC_T
 #define ___________________MOU_L2__________________       LGUI_T(KC_A),    LALT_T(KC_S),    LCTL_T(KC_D),    LSFT_T(KC_F),    HYPR_T(KC_G)

8c61d68a19f2dbddbc63e10fe28f3824c05fa019
Huy Duong <huy.duongdinh@gmail.com>
2024-09-13 22:00:59 +0700

    Caps word configuration

---
+++ users/dinhhuy258/caps_word.c
@@ -0,0 +1,20 @@
+#include "dinhhuy258.h"
+
+bool caps_word_press_user(uint16_t keycode) {
+    switch (keycode) {
+        // Keycodes that continue Caps Word, with shift applied.
+        case KC_A ... KC_Z:
+            add_weak_mods(MOD_BIT(KC_LSFT)); // Apply shift to next key.
+            return true;
+
+        case KC_1 ... KC_0:
+        case KC_BSPC:
+        case KC_DEL:
+        case KC_MINS:
+        case KC_UNDS:
+            return true;
+
+        default:
+            return false; // Deactivate Caps Word.
+    }
+}
--- users/dinhhuy258/rules.mk
+++ users/dinhhuy258/rules.mk
@@ -1,6 +1,7 @@
 include $(USER_PATH)/oled/rules.mk
 
 SRC += $(USER_PATH)/process_records.c
+SRC += $(USER_PATH)/caps_word.c
 SRC += $(USER_PATH)/swapper.c
 # https://github.com/qmk/qmk_firmware/issues/21137#issuecomment-1577898767
 INTROSPECTION_KEYMAP_C = $(USER_PATH)/combos.c

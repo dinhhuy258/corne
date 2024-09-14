5d7ca87108e766194bcdac45458febaa69ca0506
Huy Duong <huy.duongdinh@gmail.com>
2024-09-14 16:06:30 +0700

    Adjust mouse speed

--- users/dinhhuy258/config.h
+++ users/dinhhuy258/config.h
@@ -9,11 +9,11 @@
 
 #define MK_3_SPEED
 #define MK_MOMENTARY_ACCEL
-#define MK_C_OFFSET_0 4
+#define MK_C_OFFSET_0 6
 #define MK_C_INTERVAL_0 16
-#define MK_C_INTERVAL_1 8
-#define MK_C_OFFSET_1 16
-#define MK_C_OFFSET_2 40
+#define MK_C_OFFSET_1 24
+#define MK_C_INTERVAL_1 16
+#define MK_C_OFFSET_2 42
 #define MK_C_INTERVAL_2 16
 
 #define TAPPING_TERM 200
